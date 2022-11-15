85db48a3f7c9113615f2de0bdfa0e79498190271
HH7H <105313594+HH7H@users.noreply.github.com>
2022-11-16 00:14:36 +0300

    Add files via upload

---
+++ Flask.py
@@ -0,0 +1,29 @@
+from Naked.toolshed.shell import muterun_js
+import requests,json,os.path
+from flask import Flask,request
+import random
+import string
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def Root():
+    return 'Hello from Flask'
+    
+
+@app.route('/api/tiktok/sign-request/',methods=['POST'])
+def Api():
+    url = str(request.form['url'])
+    XApiKey = str(request.form['X-Api-Key'])
+    if XApiKey == 'Pass@api':
+        response = muterun_js(' '.join([os.path.abspath('browser.js'), "\""+url+"\""]))
+        signature = json.loads(response.stdout)
+        return signature
+    else:
+        return json.dump({"status": "error", "error_type": "incorrect password"})
+        
+
+        
+        
+        
+        
